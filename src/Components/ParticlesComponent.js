f65222ddf5a676b7e99317fe398abb76195a450b
Brittany Shelley <brittanyshelley.webdev@gmail.com>
2024-06-09 05:23:48 +0000

    chore: Update tsparticles dependency to version 3.4.0

---
+++ src/Components/ParticlesComponent.js
@@ -0,0 +1,15 @@
+// src/components/ParticlesComponent.js
+import React, { useCallback } from 'react';
+import Particles from '@tsparticles/react';
+import { loadFull } from 'tsparticles';
+import particlesOptions from '../particles.json';
+
+const ParticlesComponent = () => {
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  return <Particles options={particlesOptions} init={particlesInit} />;
+};
+
+export default ParticlesComponent;
